559137f96d19263ce7f8ee7a72779587f98e6d0f
Joshua Gilmore <djsounddog@gmail.com>
2020-07-17 14:21:22 +1000

    Disabled prettier as some rules incompatible with rubocop. Updated rubocop to apply all imported cops.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,1 +1,6 @@
-class ApplicationJob < ActiveJob::Base; end # discard_on ActiveJob::DeserializationError # Most jobs are safe to ignore if the underlying records are no longer available # retry_on ActiveRecord::Deadlocked # Automatically retry jobs that encountered a deadlock
+class ApplicationJob < ActiveJob::Base
+  # discard_on ActiveJob::DeserializationError
+  # Most jobs are safe to ignore if the underlying records are no longer available
+  # retry_on ActiveRecord::Deadlocked
+  # Automatically retry jobs that encountered a deadlock
+end
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ require 'rails/test_unit/railtie'
 Bundler.require(*Rails.groups)
 
 module BagukiraBackend
-  class Application < Rails::Application # Initialize configuration defaults for originally generated Rails version.
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
