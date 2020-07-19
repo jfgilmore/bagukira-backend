7f7609cec04375f4d2c20e0e6ef28da396b295ab
Joshua Gilmore <djsounddog@gmail.com>
2020-07-19 17:02:05 +1000

    Unit routes tests created, routes and actions created all passing.

---
+++ app/controllers/units_controller.rb
@@ -0,0 +1,43 @@
+class UnitsController < ApplicationController
+  before_action :set_unit, only: %i[show update delete]
+
+  def index
+    units = Unit.all
+    render json: { units: units }, status: 200
+  end
+
+  def show
+    render json: @unit, status: 200
+  end
+
+  def create
+    unit = Unit.new(unit_params)
+    if unit.save
+      render json: unit, status: 200
+    else
+      render status: 500
+    end
+  end
+
+  def update
+    if @unit.update(unit_params)
+      render json: @unit, status: 204
+    else
+      render status: 500
+    end
+  end
+
+  def delete
+    render json: {}, status: :no_content if @unit.destroy
+  end
+
+  private
+
+  def unit_params
+    params.require(:unit).permit(:email)
+  end
+
+  def set_unit
+    @unit = Unit.find(params[:id])
+  end
+end
--- spec/requests/routes_spec.rb
+++ spec/requests/routes_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe 'Routing', type: :routing do
     it { should route(:patch, '/users/1').to(controller: :users, action: :update, id: 1) }
     it { should route(:delete, '/users/1').to(controller: :users, action: :destroy, id: 1) }
   end
+
+  describe 'Unit routes' do
+    it { should route(:get, '/units').to(controller: :units, action: :index) }
+    it { should route(:get, '/units/1').to(controller: :units, action: :show, id: 1) }
+    it { should route(:post, '/units').to(controller: :units, action: :create) }
+    it { should route(:put, '/units/1').to(controller: :units, action: :update, id: 1) }
+    it { should route(:patch, '/units/1').to(controller: :units, action: :update, id: 1) }
+    it { should route(:delete, '/units/1').to(controller: :units, action: :destroy, id: 1) }
+  end
 end
