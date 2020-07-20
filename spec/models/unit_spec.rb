9fdf9baca692e4b341ebe11d4db5a8ffaa4d6aa3
Joshua Gilmore <djsounddog@gmail.com>
2020-07-21 00:55:46 +1000

    Reinstated test and validation for presence of unit_type

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Unit, type: :model do
 
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_least(3) }
-    # it { should validate_presence_of(:unit_type) }
+    it { should validate_presence_of(:unit_type) }
     it { should validate_presence_of(:user) }
   end
 
