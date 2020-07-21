98715b1bd6d9dad497b3ae33452f5e7a2064455c
Joshua Gilmore <djsounddog@gmail.com>
2020-07-21 11:58:06 +1000

    Added redirects of projects to units routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, :units
+  resources :users
+  resources :units do
+    # resources :tickets
+  end
+  match 'projects', to: 'units#index', via: :get
+  match 'projects', to: 'units#create', via: :post
+  match 'projects/:id', to: 'units#show', via: :get
+  match 'projects/:id', to: 'units#update', via: :put
+  match 'projects/:id', to: 'units#update', via: :patch
+  match 'projects/:id', to: 'units#destroy', via: :delete
 end
