4e2730baaaaf8ae40273f5b1157d2b5a655f66af
Joshua Gilmore <djsounddog@gmail.com>
2020-07-21 13:14:43 +1000

    Removed sectres from dev env config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.allowed_cors_origins = %w[http://localhost:3000]
-
-  config.PGUSER = 'bagukira_test'
-  config.PGPASSWORD = 'postgres'
 end
