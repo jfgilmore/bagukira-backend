6d7b370b15ab82f4b4d50fe11aa0b541476baac0
Joshua Gilmore <djsounddog@gmail.com>
2020-07-22 14:20:21 +1000

    Unit validation refactor to DRY up code.

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,7 +1,6 @@
 class Unit < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
-  validates :user_id, presence: true
-  validates :unit_type, presence: true
+  validates :user_id, :unit_type, presence: true
   enum unit_type: { project: 0, team: 1, organisation: 2 }
   belongs_to :user
 end
