e3758c97ad3268b71a1fdc7b4271125661335dba
Joshua Gilmore <djsounddog@gmail.com>
2020-07-22 14:20:53 +1000

    Added direct and indirect routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, :tickets
   resources :units do
-    # resources :tickets
+    resources :tickets
   end
+
   match 'projects', to: 'units#index', via: :get
   match 'projects', to: 'units#create', via: :post
   match 'projects/:id', to: 'units#show', via: :get
