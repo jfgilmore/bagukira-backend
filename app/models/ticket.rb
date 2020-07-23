df46fc38ce7cd6cf0fc9d82332a8b4b1dec0ccc5
Joshua Gilmore <djsounddog@gmail.com>
2020-07-23 23:38:41 +1000

    Added validations and foreign key

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,9 @@
 class Ticket < ApplicationRecord
+  validates :subject, :status, :unit_hash, :opened_by, presence: true
+  enum status: { open: 1, in_progress: 2, closed: 3 }
+  belongs_to :unit, primary_key: 'unit_hash', counter_cache: true
+
+  def location_id_or_other
+    errors.add(:closed_by, 'is required if status is closed') if status == :closed && closed_by.blank?
+  end
 end
