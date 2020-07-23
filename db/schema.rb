28f6c14899ad1f3cd0cb98baf988de20013b2cc8
Joshua Gilmore <djsounddog@gmail.com>
2020-07-23 23:44:23 +1000

    Misc changes: - Fixed response codes - Added project count for users - changed dependent action

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_083832) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.integer "units_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
