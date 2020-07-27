e8de7e2b5d1aa64146e0fdc6d1a7096c8507bd3e
Joshua Gilmore <djsounddog@gmail.com>
2020-07-27 10:55:04 +1000

    Added severity to bug tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,7 @@ class Ticket < ApplicationRecord
   validates :subject, :status, :unit_id, :opened_by, presence: true
   validates :closed_by, presence: true, if: :status_closed?, on: :update
   enum status: { open: 1, in_progress: 2, closed: 3 }
+  enum severity: { low: 1, medium: 2, major: 3, critical: 4 }
   belongs_to :unit, primary_key: 'unit_hash', counter_cache: true
 
   def to_param
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_062253) do
+ActiveRecord::Schema.define(version: 2020_07_27_004541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_07_26_062253) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "ticket_num"
+    t.integer "severity", default: 1, null: false
   end
 
   create_table "units", force: :cascade do |t|
@@ -44,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_07_26_062253) do
     t.integer "units_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
