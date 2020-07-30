07b49dcd0bb0503e24054d9da6686d8eb12b1446
Joshua Gilmore <djsounddog@gmail.com>
2020-07-30 17:34:18 +1000

    Removed useless comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.allowed_cors_origins = 'http://localhost:3000'
-  # config.pguser = 'bagukira_backend_development'
-  # config.pgpassword = 'postgres'
 end
