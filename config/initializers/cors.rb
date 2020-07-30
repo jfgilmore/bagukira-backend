bd1d1a4b1189ddf659dd986c2c101e6917edc132
Joshua Gilmore <djsounddog@gmail.com>
2020-07-30 19:58:23 +1000

    Hardcoded cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.config.allowed_cors_origins
+    origins 'https://bagukira.com, https://www.bagukira.com, http://bagukira.com, http://www.bagukira.com'
 
     resource '*',
              headers: :any, methods: %i[get post put patch delete options head]
