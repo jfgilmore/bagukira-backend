01bd1dcf535412e64241c5f10215fa89582f4e28
Joshua Gilmore <djsounddog@gmail.com>
2020-07-30 19:42:09 +1000

    Removed error handling from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-
-  rescue_from RecordNotFound, with: :handle_not_found
-  rescue_from DeleteRestrictionError, with: :handle_cant_destroy_dependents
-  rescue_from ParameterMissing, with: :handle_missing_parameters
-  rescue_from StandardError, with: :handle_uncaught_error
+  # rescue_from RecordNotFound, with: :handle_not_found
+  # rescue_from DeleteRestrictionError, with: :handle_cant_destroy_dependents
+  # rescue_from ParameterMissing, with: :handle_missing_parameters
+  # rescue_from StandardError, with: :handle_uncaught_error
 
   # Valid 4hrs: change in config/initializers/knock.rb .token_lifetime
   def new_jwt
@@ -29,23 +28,23 @@ class ApplicationController < ActionController::API
     current_user.present?
   end
 
-  def handle_not_found
-    render json: {}, status: :not_found
-  end
+  # def handle_not_found
+  #   render json: {}, status: :not_found
+  # end
 
-  def handle_cant_destroy_dependents
-    render json: {
-      error: 'This unit has dependents, delete all it\'s dependents before you can delete the unit itself.'
-    }, status: :forbidden
-  end
+  # def handle_cant_destroy_dependents
+  #   render json: {
+  #     error: 'This unit has dependents, delete all it\'s dependents before you can delete the unit itself.'
+  #   }, status: :forbidden
+  # end
 
-  def handle_missing_parameters
-    render json: {
-      error: 'Misformed request format { "controller": { "param": "value" } }.'
-    }, status: :forbidden
-  end
+  # def handle_missing_parameters
+  #   render json: {
+  #     error: 'Misformed request format { "controller": { "param": "value" } }.'
+  #   }, status: :forbidden
+  # end
 
-  def handle_uncaught_error(obj)
-    render json: { errors: obj }, status: :internal_server_error
-  end
+  # def handle_uncaught_error(obj)
+  #   render json: { errors: obj }, status: :internal_server_error
+  # end
 end
