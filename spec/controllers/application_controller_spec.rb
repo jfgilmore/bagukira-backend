ddd57b77997b7858d1d7379a5d154a5e05dfaf5b
Joshua Gilmore <djsounddog@gmail.com>
2020-07-30 19:34:02 +1000

    Updated test spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe ApplicationController, type: :controller do
   describe 'error handling' do
     # it { should rescue_from(StandardError).with(:handle_uncaught_error) }
-    it { should rescue_from(ActiveRecord::RecordNotFound).with(:handle_not_found) }
-    it { should rescue_from(ActiveRecord::DeleteRestrictionError).with(:handle_cant_destroy_dependents) }
-    # it { should rescue_from(ActionController::ParameterMissing).with(:handle_missing_parameters) }
+    it { should rescue_from(RecordNotFound).with(:handle_not_found) }
+    it { should rescue_from(DeleteRestrictionError).with(:handle_cant_destroy_dependents) }
+    # it { should rescue_from(ParameterMissing).with(:handle_missing_parameters) }
     it { should { respond_with(:forbidden) || respond_with(:not_found) || respond_with(:internal_server_error) } }
   end
 end
